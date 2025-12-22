f67a26a5605509bfbd4775f62c1c4ad16aba7322
AbdulKreemShah2408 <abdulkreemshah92@gmail.com>
2025-12-22 10:53:30 -0800

    fix fronted server.js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
-export const server="http://localhost:8000/api/v2";
+export const server = (import.meta.env.VITE_BACKEND_URL ? import.meta.env.VITE_BACKEND_URL + "/api/v2" : "http://localhost:8000/api/v2");
 
-export const backend_url="http://localhost:8000/"
+export const backend_url = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000/";
